b00681bf207132ac2163bae3e61f4223dda1e8fd
SpaceWork1123 <spacework54466@gmail.com>
2025-05-14 16:20:25 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AutoPriceCalculator from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AutoPriceCalculator />
-  </React.StrictMode>
-);
